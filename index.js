31a9b30f8d26962be117977ceec1d89e81b9fdb2
Ismail Chaiq <CHAIQ.ISMAIL@ofppt-edu.ma>
2022-01-04 22:56:16 +0100

    Fix bug expo main component

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+ import { AppRegistry, Platform } from 'react-native';
+ import App from './App';
+ 
+ AppRegistry.registerComponent('main', () => App);
+ 
+ if (Platform.OS === 'web') {
+     const rootTag = document.getElementById('root') || document.getElementById('X');
+     AppRegistry.runApplication('X', { rootTag });
+ }
