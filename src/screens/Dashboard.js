e76d881a7390fcbccec29deaa8cd1dd5290e41a7
Ismail Chaiq <CHAIQ.ISMAIL@ofppt-edu.ma>
2022-01-10 00:51:38 +0100

    sync

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -12,38 +12,118 @@ import Header from '../components/Header';
 import Paragraph from '../components/Paragraph';
 import Button from '../components/Button';
 
-import {TextInput} from 'react-native-paper'
-import { View,Text } from 'react-native';
+import {TextInput,Appbar} from 'react-native-paper'
+import { View,Text,StyleSheet,Dimensions,ScrollView,SafeAreaView } from 'react-native';
+
+
+import{LineChart} from 'react-native-chart-kit';
 
 import {NavigationContainer} from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator  } from '@react-navigation/drawer';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator ();
 
 export default function Dashboard({ route,navigation }) {
+
+  const styles = StyleSheet.create({
+    top: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: '5%',
+    },
+    chart:{
+      marginTop:'25%',
+      alignItems:'center',
+    }
+  });
+
+  // const openDrawer = () => {
+  //   console.log("test");
+  // }
+
   function HomeScreen() {
     // console.warn(JSON.stringify(route.params));
     const [text, setText] = React.useState('');
 		return (
       <View>
-        <View style={{flexDirection:'row'}}>
-          <Button style={{width:'10%'}} icon='bing'></Button>
-          <TextInput style={{width:'80%',height:'5%'}}
-            label="Search"
-            value={text}
-            onChangeText={text => setText(text)}
-          />
-        </View>
+    <Appbar.Header style={styles.top}>
+      <Appbar.Action
+      icon="archive"
+       onPress={() => {console.warn('Pressed archive')}}
+       style={{
+         height:48,
+         width:48
+       }}
+      />
+      <Appbar.Action
+      icon="archive"
+       onPress={() => {console.warn('Pressed archive')}}
+       style={{
+         height:48,
+         width:48
+       }}
+      />
+      <Appbar.Content title="Dashboard" />
+      {/* <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} /> */}
+      {/* <Appbar.Action
+      icon="delete"
+      onPress={() => console.log('Pressed delete')}
+      /> */}
+      </Appbar.Header>
+      <SafeAreaView>
+      <ScrollView>
+      <View style={styles.chart}>
+      <Text style=''>Bezier Line Chart</Text>
+      <LineChart
+        data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 1,
+            Math.random() * 1,
+            Math.random() * 1,
+            Math.random() * 1,
+            Math.random() * 1,
+            Math.random() * 1
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={128}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      margin:16,
+      borderRadius: 18
+        }}
+      />
+    </View>
+      </ScrollView>
+      </SafeAreaView>
+    
       </View>
-
-
-			// <View style={{ 
-			// 	flex: 1,
-			// 	alignItems: 'center',
-			// 	justifyContent: 'center' 
-			//   }}>
-			//   <Text>{route.params.user}</Text>
-			// </View>
 		);
 		}
 		 
@@ -57,31 +137,19 @@ export default function Dashboard({ route,navigation }) {
 		  }
 
   return (
-    // <NavigationContainer>
-      <Tab.Navigator screenOptions={{
+    
+    
+      <Drawer.Navigator screenOptions={{
         headerShown: false,
       }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    // </NavigationContainer>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+  
     
-    //  <Logo />
-    //   <Header>Let’s start</Header>
-    //   <Paragraph>
-    //     Your amazing app starts here. Open you favorite code editor and start
-    //     editing this project.
-    //   </Paragraph>
-    //   <Button
-    //     mode="outlined"
-    //     onPress={() =>
-    //       navigation.reset({
-    //         index: 0,
-    //         routes: [{ name: 'StartScreen' }],
-    //       })
-    //     }
-    //   >
-    //     Logout
-    //   </Button> 
+    
+ 
   )
+
+  
 }
